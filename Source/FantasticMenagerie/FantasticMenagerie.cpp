fcf124b597df031b2ce7fcb323cf89ff074dcc7e
Robin Kruck <Robin.Kruck93@web.de>
2023-06-09 19:10:58 +0200

    Fantastic Menagerie

---
+++ Source/FantasticMenagerie/FantasticMenagerie.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FantasticMenagerie.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FantasticMenagerie, "FantasticMenagerie" );
---
+++ Source/FantasticMenagerie/FantasticMenagerieGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FantasticMenagerieGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FANTASTICMENAGERIE_API AFantasticMenagerieGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
